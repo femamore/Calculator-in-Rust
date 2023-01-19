ca551aae3d7ca6ce359ae2e1aecc4b7b0228d8de
Celeste <>
2023-01-18 20:33:11 -0600

    testing new features

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,117 @@
-#![allow(unused_imports)]
-
-use std::path::PathBuf;
+#![allow(non_camel_case_types)]
 
 use clap::{Parser, Subcommand, Command};
+use std::str::SplitWhitespace;
+
 
 #[derive(Parser)]
 #[clap(about, version, author, long_about = None)]
 struct Value {
     #[clap(subcommand)]
-    command: Commands,
+    command: Option<Commands>,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Add numbers 
-    Add { number_one: i32, number_two: i32 },
-    /// Subtract numbers
-    Subtract { number_one: i32, number_two: i32 },
-    /// Multiply numbers
-    Multiply { number_one: i32, number_two: i32 },
-    /// Divide numbers
-    Divide { number_one: i32, number_two: i32 },
+    /// calculate numbers 
+    calc { 
+     #[clap(name = "EQUATION")]
+     equation: String, 
+     },
 }
-    
+
 
 fn main() {
      let value = Value::parse();
 
-     match &value.command {
-           Commands::Add { number_one, number_two } => {
-                println!("The answer is: {}!", number_one + number_two)
-           }
-           Commands::Subtract { number_one, number_two } => {
-                println!("The answer is: {}!", number_one - number_two)
-           }
-           Commands::Multiply { number_one, number_two } => {
-                println!("The answer is: {}!", number_one * number_two)
-           }
-           Commands::Divide { number_one, number_two } => {
-                println!("The answer is: {}!", number_one / number_two)
-           }
-      }
+     if let Some(Commands::calc { equation }) = value.command {
+          println!("{}", parse_equation(&equation))
+     } else {
+          let mut equation = String::new();
+          println!("Enter an equation: ");
+          std::io::stdin().read_line(&mut equation).expect("Failed to read line");
+          println!("{}", parse_equation(&equation.trim()));
+     }
 }
+
+fn parse_equation(equation: &str) -> i32 {
+     let mut tokens = equation.split_whitespace();
+     let mut result = parse_token(&mut tokens);
+     
+     while let Some(token) = tokens.next() {
+          match token {
+               "+" => result += parse_token(&mut tokens),
+               "-" => result -= parse_token(&mut tokens),
+               _ => break,
+          }
+     }
+
+     result
+}
+
+fn parse_token(tokens: &mut SplitWhitespace) -> i32 {
+     let mut result = parse_number(tokens);
+
+     loop {
+          let token = tokens.next();
+          match token {
+               Some("*") => result *= parse_number(tokens),
+               Some("/") => result /= parse_number(tokens),
+               Some("+") | Some("-") => return result,
+               _ => break,
+          }
+     }
+     result
+}
+
+fn parse_number(tokens: &mut SplitWhitespace) -> i32 {
+     let token = tokens.next().unwrap();
+     match token {
+          "+" => parse_number(tokens),
+          "-" => -parse_number(tokens),
+          _ => token.parse().unwrap(),
+     }
+}
+
+/* 
+fn parse_expression(tokens: &mut SplitWhitespace) -> i32 {
+     let mut result = parse_term(tokens);
+
+     loop {
+           let token = tokens.next(); 
+           match token {
+                Some("+") => result += parse_term(tokens),
+                Some("-") => result -= parse_term(tokens),
+                _ => break,
+           }
+     }
+
+     result
+}
+
+fn parse_term(tokens: &mut SplitWhitespace) -> i32 {
+     let mut result = parse_factor(tokens);
+
+     loop {
+           let token = tokens.next(); 
+           match token {
+                Some("*") => result *= parse_factor(tokens),
+                Some("/") => result /= parse_factor(tokens),
+                _ => break,
+           }
+     }
+
+     result
+}
+
+fn parse_factor(tokens: &mut SplitWhitespace) -> i32 {
+     let token = tokens.next().unwrap();
+     match token {
+          "+" => parse_factor(tokens),
+          "-" => -parse_factor(tokens),
+          "*" => parse_factor(tokens) * parse_factor(tokens),
+          "/" => parse_factor(tokens) / parse_factor(tokens),
+          _ => token.parse().unwrap(),
+     }
+}
+*/
